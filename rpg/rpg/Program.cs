e73bbd49f0c2c2fb816199cad70e914cf1a652e7
QuentinLB <q.lebour@gmail.com>
2014-05-04 20:07:06 +0200

    base classes

---
+++ rpg/rpg/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // serialisation
+            //Story story = new Story();
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    story.add_link(new NodeLink(new StoryNode(), new StoryNode(), new Fight()));
+            //}
+            //FileStream fs = new FileStream("test.xml", FileMode.OpenOrCreate);
+            //XmlSerializer s = new XmlSerializer(typeof(Story), new Type[]{typeof(NodeLink), typeof(StoryNode), typeof(Fight)});
+            //s.Serialize(fs, story);
+
+            // deserialisation
+            XmlSerializer sss = new XmlSerializer(typeof(Story), new Type[]{typeof(NodeLink), typeof(StoryNode), typeof(Fight)});
+            StreamReader reader = new StreamReader("test.xml");
+            Story s2;
+
+            s2 = (Story)sss.Deserialize(reader);
+            reader.Close();
+
+
+        }
+    }
+}
---
+++ rpg/rpg/Story/Story.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+[Serializable]
+[XmlRoot("Story")]
+public class Story
+{
+    [XmlArray("NodeLinkList"), XmlArrayItem(typeof(NodeLink), ElementName = "NodeLink")]
+    public List<NodeLink> links { get; set; }
+
+    public Story()
+    {
+        links = new List<NodeLink>();
+    }
+
+    public void add_link(NodeLink n){
+        links.Add(n);
+    }
+}
